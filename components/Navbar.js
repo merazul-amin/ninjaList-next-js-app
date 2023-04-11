d28b9f7c34649b77c9bdf60a5feefb9adc165a54
Merazul Amin Saikut <merazulamin2122@yahoo.com>
2023-04-11 10:46:30 +0600

    Image and Head component added

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,3 +1,4 @@
+import Image from 'next/image';
 import Link from 'next/link';
 import React from 'react';
 
@@ -5,7 +6,7 @@ const Navbar = () => {
     return (
         <nav>
             <div>
-                <h1>Ninja List</h1>
+                <Image src={'/logo.png'} width={100} height={100}></Image>
             </div>
             <Link href="/">Home</Link>
             <Link href="/about">About</Link>
--- pages/about.js
+++ pages/about.js
@@ -1,10 +1,17 @@
+import Head from 'next/head';
 import React from 'react';
 
 const About = () => {
     return (
-        <div>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut consequuntur accusamus quos ratione deserunt assumenda impedit praesentium perferendis unde, sequi fugit, veritatis hic. Vel, hic autem explicabo fuga ipsam doloribus!</p>
-        </div>
+        <>
+            <Head>
+                <title>About Page | About</title>
+                <meta name="keywords" content="ninjas" />
+            </Head>
+            <div>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut consequuntur accusamus quos ratione deserunt assumenda impedit praesentium perferendis unde, sequi fugit, veritatis hic. Vel, hic autem explicabo fuga ipsam doloribus!</p>
+            </div>
+        </>
     );
 };
 
