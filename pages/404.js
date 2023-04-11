1d39739e1f7ac06e96f2cae3716a992cc4ad5bd3
Merazul Amin Saikut <merazulamin2122@yahoo.com>
2023-04-11 10:30:05 +0600

    custom error page added

---
+++ pages/404.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const ErrorPage = () => {
+    return (
+        <div>
+            <h1>This is Error page. And this url dose not exist.</h1>
+        </div>
+    );
+};
+
+export default ErrorPage;
