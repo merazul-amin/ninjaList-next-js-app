361289f665b28cf91fa6ac373ce0e00fb6b9a80e
Merazul Amin Saikut <merazulamin2122@yahoo.com>
2023-04-11 10:34:55 +0600

    useRouter hook implemented

--- pages/404.js
+++ pages/404.js
@@ -1,6 +1,15 @@
-import React from 'react';
+import { useRouter } from 'next/router';
+import React, { useEffect } from 'react';
+
+
 
 const ErrorPage = () => {
+    const router = useRouter();
+    useEffect(() => {
+        setTimeout(() => {
+            router.push('/');
+        }, 3000)
+    }, [])
     return (
         <div>
             <h1>This is Error page. And this url dose not exist.</h1>
