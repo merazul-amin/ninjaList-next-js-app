3ee7efa197ed30658942f76e56bc606784dc7c82
Merazul Amin Saikut <merazulamin2122@yahoo.com>
2023-04-11 11:46:59 +0600

    getStaticProps function added for server side rendering

--- pages/ninjas/index.js
+++ pages/ninjas/index.js
@@ -1,9 +1,23 @@
-import React from 'react';
+import Link from "next/link";
+import styles from '@/styles/Ninjas.module.css';
 
-const NinjaHome = () => {
+export const getStaticProps = async () => {
+    const res = await fetch('https://jsonplaceholder.typicode.com/users');
+    const data = await res.json();
+    return {
+        props: {
+            users: data
+        }
+    }
+}
+
+
+const NinjaHome = ({ users }) => {
     return (
         <div>
             <h1>All Ninjas</h1>
+            {users?.length}
+            {users?.map(user => <Link href={'/'} className={styles.single}>{user.name}</Link>)}
         </div>
     );
 };
