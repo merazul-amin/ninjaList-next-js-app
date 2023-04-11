ef6be831fdbd979828bba5b2cb9790668bef8dc3
Merazul Amin Saikut <merazulamin2122@yahoo.com>
2023-04-11 09:52:52 +0600

    components added

---
+++ components/Navbar.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Navbar = () => {
+    return (
+        <nav>
+            <div>
+                <h1>Ninja List</h1>
+            </div>
+            <a href="">Home</a>
+            <a href="">About</a>
+            <a href="">Nina Listing</a>
+        </nav>
+    );
+};
+
+export default Navbar;
--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,15 @@
-import Head from 'next/head'
-import Image from 'next/image'
 import { Inter } from 'next/font/google'
-import styles from '@/styles/Home.module.css'
+import Navbar from '@/components/Navbar'
+import Footer from '@/components/Footer'
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
     <>
+      <Navbar />
       <h1>Hello World</h1>
-
+      <Footer />
     </>
   )
 }
